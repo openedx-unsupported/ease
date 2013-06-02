80a80dc3f5a80b233f45a5ba56de138c7db73fc2
Hugh Brown <hbrown@wgen.net>
2013-06-02 19:28:34 -0400

    ./model_creator.py: E128 continuation line under-indented for visual indent

--- ease/model_creator.py
+++ ease/model_creator.py
@@ -120,14 +120,14 @@ def get_algorithms(type):
     """
     if type == util_functions.AlgorithmTypes.classification:
         clf = sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learn_rate=.05,
-            max_depth=4, random_state=1, min_samples_leaf=3)
+                                                          max_depth=4, random_state=1, min_samples_leaf=3)
         clf2 = sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learn_rate=.05,
-            max_depth=4, random_state=1, min_samples_leaf=3)
+                                                           max_depth=4, random_state=1, min_samples_leaf=3)
     else:
         clf = sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learn_rate=.05,
-            max_depth=4, random_state=1, min_samples_leaf=3)
+                                                         max_depth=4, random_state=1, min_samples_leaf=3)
         clf2 = sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learn_rate=.05,
-            max_depth=4, random_state=1, min_samples_leaf=3)
+                                                          max_depth=4, random_state=1, min_samples_leaf=3)
     return clf, clf2
 
 

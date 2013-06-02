14fdf0cb5edfab99eca1cccaa13d09de676ce51f
Hugh Brown <hbrown@wgen.net>
2013-06-02 19:27:10 -0400

    ./essay_set.py: E231 Add missing whitespace.

--- ease/essay_set.py
+++ ease/essay_set.py
@@ -71,9 +71,9 @@ class EssaySet(object):
             essay_text = str(essay_text)
         except:
             #Nothing needed here, will return error in any case.
-            log.exception("Invalid type for essay score : {0} or essay text : {1}".format(type(essay_score),type(essay_text)))
+            log.exception("Invalid type for essay score : {0} or essay text : {1}".format(type(essay_score), type(essay_text)))
 
-        if isinstance(essay_score,int) and isinstance(essay_text, basestring)\
+        if isinstance(essay_score, int) and isinstance(essay_text, basestring)\
         and (essay_generated == 0 or essay_generated == 1):
             self._id.append(max_id + 1)
             self._score.append(essay_score)
@@ -81,13 +81,13 @@ class EssaySet(object):
             try:
                 essay_text = str(essay_text.encode('ascii', 'ignore'))
             except:
-                essay_text = (essay_text.decode('utf-8','replace')).encode('ascii','ignore')
+                essay_text = (essay_text.decode('utf-8', 'replace')).encode('ascii', 'ignore')
             cleaned_essay = util_functions.sub_chars(essay_text).lower()
             if(len(cleaned_essay) > MAXIMUM_ESSAY_LENGTH):
                 cleaned_essay = cleaned_essay[0:MAXIMUM_ESSAY_LENGTH]
             self._text.append(cleaned_essay)
             # Spell correct text using aspell
-            cleaned_text,spell_errors,markup_text = util_functions.spell_correct(self._text[len(self._text) - 1])
+            cleaned_text, spell_errors, markup_text = util_functions.spell_correct(self._text[len(self._text) - 1])
             self._clean_text.append(cleaned_text)
             self._spelling_errors.append(spell_errors)
             self._markup_text.append(markup_text)

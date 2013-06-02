98cd110e1e4a3d9a3c680cbde63cc5bbcf284148
Hugh Brown <hbrown@wgen.net>
2013-06-02 19:27:00 -0400

    ./predictor_set.py: E225 Fix missing whitespace around operator.

--- ease/predictor_set.py
+++ ease/predictor_set.py
@@ -11,9 +11,9 @@ sys.path.append(base_path)
 import util_functions
 
 if not base_path.endswith("/"):
-    base_path=base_path+"/"
+    base_path = base_path+"/"
 
-log=logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class PredictorSet(object):
@@ -25,10 +25,10 @@ class PredictorSet(object):
             type = "train"
 
         self._type = type
-        self._target=[]
-        self._textual_features=[]
-        self._numeric_features=[]
-        self._essay_sets=[]
+        self._target = []
+        self._textual_features = []
+        self._numeric_features = []
+        self._essay_sets = []
 
     def add_row(self, numeric_features, textual_features, target):
         #Basic input checking
@@ -48,7 +48,7 @@ class PredictorSet(object):
             raise util_functions.InputError(textual_features, error_message)
 
         #Do some length checking for parameters
-        if len(self._numeric_features)>0:
+        if len(self._numeric_features) > 0:
             numeric_length = len(self._numeric_features[-1])
             current_numeric_length = len(numeric_features)
             if numeric_length != current_numeric_length:
@@ -56,7 +56,7 @@ class PredictorSet(object):
                 log.exception(error_message)
                 raise util_functions.InputError(numeric_features, error_message)
 
-        if len(self._textual_features)>0:
+        if len(self._textual_features) > 0:
             textual_length = len(self._textual_features[-1])
             current_textual_length = len(textual_features)
             if textual_length != current_textual_length:
@@ -83,7 +83,7 @@ class PredictorSet(object):
                 raise util_functions.InputError(textual_features, error_message)
 
         #Create essay sets for textual features if needed
-        if len(self._textual_features)==0:
+        if len(self._textual_features) == 0:
             for i in xrange(0,len(textual_features)):
                 self._essay_sets.append(essay_set.EssaySet(type=self._type))
 

889b5bb3ae29b0800025629b54abd6662f032363
Vik Paruchuri <vik@edx.org>
2013-06-06 15:39:51 -0400

    Fix confidence generation

--- ease/grade.py
+++ ease/grade.py
@@ -175,14 +175,16 @@ def get_confidence_value(algorithm,model,grader_feats,score, scores):
     """
     min_score=min(numpy.asarray(scores))
     max_score=max(numpy.asarray(scores))
-    if algorithm == util_functions.AlgorithmTypes.classification:
+    if algorithm == util_functions.AlgorithmTypes.classification and hasattr(model, "predict_proba"):
         #If classification, predict with probability, which gives you a matrix of confidences per score point
         raw_confidence=model.predict_proba(grader_feats)[0,(float(score)-float(min_score))]
         #TODO: Normalize confidence somehow here
         confidence=raw_confidence
-    else:
+    elif hasattr(model, "predict"):
         raw_confidence = model.predict(grader_feats)[0]
         confidence = max(float(raw_confidence) - math.floor(float(raw_confidence)), math.ceil(float(raw_confidence)) - float(raw_confidence))
+    else:
+        confidence = 0
 
     return confidence
 

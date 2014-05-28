d44b446ae4ed659a79cca8d9a8678ff3c52324d3
Will Daly <will@edx.org>
2014-05-28 10:53:10 -0400

    Add requirements to setup.py; bump version number

--- setup.py
+++ setup.py
@@ -1,11 +1,37 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+')
+    )
+
+
+REQUIREMENTS = [
+    line.strip() for line in (
+        open("pre-requirements.txt").readlines() +
+        open("requirements.txt").readlines() +
+        open("base_requirements.txt").readlines()
+    )
+    if is_requirement(line)
+]
+
 
 setup(
     name = "ease",
-    version = "0.1.1",
+    version = "0.1.2",
     packages=['ease', 'ease.external_code', 'ease.data', 'ease.data.nltk_data'],
     package_data = {
         '': ['*.txt', '*.rst', '*.p', '*.zip'],
@@ -17,4 +43,5 @@ setup(
     keywords = "ml machine learning nlp essay education",
     url = "https://github.com/edx/ease",
     include_package_data = True,
-    )
+    install_requires=REQUIREMENTS,
+)

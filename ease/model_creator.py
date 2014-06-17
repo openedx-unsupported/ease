f11737af630196ad1153a1cbad403627a748d363
Will Daly <will@edx.org>
2014-06-17 15:01:50 -0400

    Upgrade scikit to 0.14.1

--- ease/model_creator.py
+++ ease/model_creator.py
@@ -116,14 +116,14 @@ def get_algorithms(algorithm):
     type - one of util_functions.AlgorithmTypes
     """
     if algorithm == util_functions.AlgorithmTypes.classification:
-        clf = sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learn_rate=.05,
+        clf = sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learning_rate=.05,
             max_depth=4, random_state=1,min_samples_leaf=3)
-        clf2=sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learn_rate=.05,
+        clf2=sklearn.ensemble.GradientBoostingClassifier(n_estimators=100, learning_rate=.05,
             max_depth=4, random_state=1,min_samples_leaf=3)
     else:
-        clf = sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learn_rate=.05,
+        clf = sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learning_rate=.05,
             max_depth=4, random_state=1,min_samples_leaf=3)
-        clf2=sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learn_rate=.05,
+        clf2=sklearn.ensemble.GradientBoostingRegressor(n_estimators=100, learning_rate=.05,
             max_depth=4, random_state=1,min_samples_leaf=3)
     return clf, clf2
 

7cdffe5b834a6fd702f3d8a0e89b7e9c71b017d6
David Baumgold <david@davidbaumgold.com>
2015-05-08 11:05:47 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name = "ease",
-    version = "0.1.2",
+    version = "0.1.3",
     packages=['ease'],
     package_data = {
         '': ['*.txt', '*.rst', '*.p', '*.zip'],

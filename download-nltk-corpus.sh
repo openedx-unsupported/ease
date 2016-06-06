503986a6d30a281a86eba9fb6517b34bca832fa6
Muzaffar Yousaf <muzaffar.yousaf@arbisoft.com>
2016-06-06 15:34:43 +0500

    Upgrade numpy to it's latest version

--- download-nltk-corpus.sh
+++ download-nltk-corpus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHON=`which python`
-sudo $PYTHON -m nltk.downloader stopwords maxent_treebank_pos_tagger wordnet -d /usr/local/share/nltk_data
+$PYTHON -m nltk.downloader stopwords maxent_treebank_pos_tagger wordnet -d /home/travis/nltk_data

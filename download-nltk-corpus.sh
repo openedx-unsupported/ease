943d3148dccb0d8e6b4f58bc7df1346ff05eb24e
Eric Herrera <eric.herrera@edunext.co>
2020-05-12 20:57:55 -0500

    Allow failures for pyton3.8. Add aspell-en. Add punkt package to fix tests.

--- download-nltk-corpus.sh
+++ download-nltk-corpus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHON=`which python`
-sudo $PYTHON -m nltk.downloader stopwords maxent_treebank_pos_tagger wordnet -d /usr/local/share/nltk_data
+sudo $PYTHON -m nltk.downloader stopwords maxent_treebank_pos_tagger wordnet punkt -d /usr/local/share/nltk_data
